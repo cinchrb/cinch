a252330952e7a7f9d8dee6724ecdb24e8c173486
Lee Jarvis <injekt.me@gmail.com>
2010-04-26 22:16:06 +0100

    version 0.2

--- lib/cinch.rb
+++ lib/cinch.rb
@@ -9,7 +9,7 @@ require 'cinch/rules'
 require 'cinch/base'
 
 module Cinch
-  VERSION = '0.1'
+  VERSION = '0.2'
 
   # Setup bot options and return a new Cinch::Base instance
   def self.setup(ops={}, &blk)
