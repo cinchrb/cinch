b29b0df4a7061046aa276ab417c4639d03621f07
Lee Jarvis <injekt.me@gmail.com>
2010-08-02 00:22:29 +0100

    initial spec skeleton

--- spec/irc/helper.rb
+++
@@ -1,8 +0,0 @@
-spec = File.dirname(__FILE__)
-$LOAD_PATH.unshift(spec) unless $LOAD_PATH.include?(spec)
-
-lib = File.dirname(__FILE__) + '../../lib/'
-$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-
-require 'cinch/irc'
-
