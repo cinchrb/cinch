5fbc04a38b64c52f8adab4ce36e3bfdf2b366775
Dominik Honnef <dominikh@fork-bomb.org>
2010-08-02 23:27:01 +0200

    rewrote memo basic example

--- examples/basic/memo.rb
+++ examples/basic/memo.rb
@@ -2,39 +2,41 @@
 
 require 'cinch'
 
-bot = Cinch.setup do
-  server "irc.freenode.org"
-  channels %w( #cinch )
-end
-
 class Memo < Struct.new(:nick, :channel, :text, :time)
   def to_s
     "[#{time.asctime}] <#{channel}/#{nick}> #{text}"
   end
 end
 
-@memos = {}
+bot = Cinch::Bot.new do
+  configure do |c|
+    c.server = "irc.freenode.org"
 
-bot.on :privmsg do |m|
-  if @memos.has_key?(m.nick)
-    bot.privmsg m.nick, @memos[m.nick].to_s
-    @memos.delete(m.nick)
+    @memos = {}
+  end
+
+  on :connect do
+    bot.join "#cinch"
+  end
+
+  on :message do |m|
+    if @memos.has_key?(m.user.nick)
+      m.user.send @memos.delete(m.user.nick).to_s
+    end
+  end
+
+  on :message, /^!memo (.+?) (.+)/ do |m, nick, message|
+    if @memos.key?(nick)
+      m.reply "There's already a memo for #{nick}. You can only store one right now"
+    elsif nick == m.user.nick
+      m.reply "You can't leave memos for yourself.."
+    elsif nick == bot.nick
+      m.reply "You can't leave memos for me.."
+    else
+      @memos[nick] = Memo.new(m.user.nick, m.channel, message, Time.now)
+      m.reply "Added memo for #{nick}"
+    end
   end
 end
 
-bot.plugin("memo :nick-string :memo") do |m|
-  nick = m.args[:nick]
-
-  if @memos.key?(nick)
-    m.reply "There's already a memo #{nick}. You can only store one right now"
-  elsif nick == m.nick
-    m.reply "You can't leave memos for yourself.."
-  elsif nick == bot.options.nick
-    m.reply "You can't leave memos for me.."
-  else
-    @memos[nick] = Memo.new(m.nick, m.channel, m.args[:memo], Time.new)
-    m.reply "Added memo for #{nick}"
-  end
-end
-
-bot.run
+bot.start
