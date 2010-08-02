016015094c9384bf69e9569d98f7dad17f3fe682
Dominik Honnef <dominikh@fork-bomb.org>
2010-08-02 02:29:00 +0200

    fix Cinch.filter_string

--- lib/cinch.rb
+++ lib/cinch.rb
@@ -4,6 +4,6 @@ module Cinch
   # @todo Handle mIRC color codes more gracefully.
   # @api private
   def self.filter_string(string)
-    s.gsub(/[\x00-\x1f]/)
+    string.gsub(/[\x00-\x1f]/, '')
   end
 end
