ca76ba418362122752ab1d98b499167a9fd29f33
Dominik Honnef <dominikh@fork-bomb.org>
2010-08-02 23:26:49 +0200

    rewrote google basic example

--- examples/basic/google.rb
+++ examples/basic/google.rb
@@ -3,29 +3,36 @@ require 'open-uri'
 require 'nokogiri'
 require 'cgi'
 
-bot = Cinch.setup do 
-  server "irc.freenode.net"
-  nick "MrCinch"
-  channels %w/ #cinch /
+bot = Cinch::Bot.new do
+  configure do |c|
+    c.server = "irc.freenode.net"
+    c.nick   = "MrCinch"
+  end
+
+  on :connect do
+    bot.join "#cinch"
+  end
+
+  helpers do
+    # Extremely basic method, grabs the first result returned by Google
+    # or "No results found" otherwise
+    def google(query)
+      url = "http://www.google.com/search?q=#{CGI.escape(query)}"
+      res = Nokogiri::HTML(open(url)).at("h3.r")
+
+      title = res.text
+      link = res.at('a')[:href]
+      desc = res.at("./following::div").children.first.text
+    rescue
+      "No results found"
+    else
+      CGI.unescape_html "#{title} - #{desc} (#{link})"
+    end
+  end
+
+  on :message, /^!google (.+)/ do |m, query|
+    m.reply google(query)
+  end
 end
 
-# Extremely basic method, grabs the first result returned by Google
-# or "No results found" otherwise
-def google(query)
-  url = "http://www.google.com/search?q=#{CGI.escape(query)}"
-  res = Nokogiri::HTML(open(url)).at("h3.r")
-
-  title = res.text
-  link = res.at('a')[:href]
-  desc = res.at("./following::div").children.first.text 
-rescue 
-  "No results found"
-else
-  CGI.unescape_html "#{title} - #{desc} (#{link})"
-end
-
-bot.plugin("google :query") do |m|
-  m.reply google(m.args[:query])
-end
-
-bot.run
+bot.start
