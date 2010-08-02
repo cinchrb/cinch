c20c46c3936437a5833711eb3f42e9174dd64c1e
Dominik Honnef <dominikh@fork-bomb.org>
2010-08-02 19:17:18 +0200

    updated basic/autovoice example

--- examples/basic/autovoice.rb
+++ examples/basic/autovoice.rb
@@ -1,32 +1,34 @@
 require 'cinch'
 
-# Give this bot ops in a channel and it'll auto voice 
+# Give this bot ops in a channel and it'll auto voice
 # visitors
 #
 # Enable with !autovoice on
 # Disable with !autovoice off
 
-bot = Cinch.setup do
-  server "irc.freenode.org"
-  channels %w( #cinch )
-end
- 
-autovoice = true
+bot = Cinch::Bot.new do
+  configure do |c|
+    c.nick            = "cinch_autovoice"
+    c.server          = "irc.freenode.org"
+    c.verbose         = true
+    @autovoice        = true
+  end
 
-bot.on :join do |m|
-  unless m.nick == bot.options.nick # We shouldn't attempt to voice ourselves
-    bot.mode(m.channel, '+v', m.nick) if autovoice
+  on :connect do
+    bot.join "#dominikh"
+  end
+
+  on :join do |m|
+    unless m.user.nick == bot.nick # We shouldn't attempt to voice ourselves
+      m.channel.voice(m.user) if @autovoice
+    end
+  end
+
+  on :channel, /^!autovoice (on|off)/ do |m, option|
+    @autovoice = option == "on"
+
+    m.reply "Autovoice is now #{@autovoice ? 'enabled' : 'disabled'}"
   end
 end
 
-bot.add_custom_pattern(:onoff, '(on|off)')
-
-bot.plugin "autovoice :option-onoff" do |m|
-  case m.args[:option]
-    when 'on'; autovoice = true
-    when 'off'; autovoice = false
-  end
-  m.answer "Autovoice is now #{autovoice ? 'enabled' : 'disabled'}"
-end
-
-bot.run
+bot.start
