73158b0717b56dc78de6a69023e23c911504826f
Dominik Honnef <dominikh@fork-bomb.org>
2010-08-04 05:40:54 +0200

    added Channel() and User() helper methods to Callback context

--- lib/cinch/callback.rb
+++ lib/cinch/callback.rb
@@ -9,5 +9,23 @@ module Cinch
     def call(message, *args, block)
       instance_exec(message, *args, &block)
     end
+
+    # Helper method for turning a String into a {Channel} object.
+    #
+    # @param (see Bot#Channel)
+    # @return (see Bot#Channel)
+    # @example (see Bot#Channel)
+    def Channel(*args)
+      @bot.Channel(*args)
+    end
+
+    # Helper method for turning a String into an {User} object.
+    #
+    # @param (see Bot#User)
+    # @return (see Bot#User)
+    # @example (see Bot#User)
+    def User(*args)
+      @bot.User(*args)
+    end
   end
 end
