9c8c6ed52e3395f5f1438bc9b89480c111aaaff5
Dominik Honnef <dominikh@fork-bomb.org>
2010-08-04 13:18:17 +0200

    added autovoice plugin example

---
+++ examples/plugins/autovoice.rb
@@ -0,0 +1,40 @@
+require 'cinch'
+
+# Give this bot ops in a channel and it'll auto voice
+# visitors
+#
+# Enable with !autovoice on
+# Disable with !autovoice off
+
+class Autovoice
+  include Cinch::Plugin
+  listen_to :join
+  match /autovoice (on|off)/
+
+  def listen(m)
+    unless m.user.nick == bot.nick
+      m.channel.voice(m.user) if @autovoice
+    end
+  end
+
+  def execute(m, option)
+    @autovoice = option == "on"
+
+    m.reply "Autovoice is now #{@autovoice ? 'enabled' : 'disabled'}"
+  end
+end
+
+bot = Cinch::Bot.new do
+  configure do |c|
+    c.nick            = "cinch_autovoice"
+    c.server          = "irc.freenode.org"
+    c.verbose         = true
+    c.plugins.plugins = [Autovoice]
+  end
+
+  on :connect do
+    bot.join "#cinch"
+  end
+end
+
+bot.start
