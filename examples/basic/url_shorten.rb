7a739c6342d2d49520436053cb4c5eb26aef010b
Dominik Honnef <dominikh@fork-bomb.org>
2010-08-04 05:40:13 +0200

    rewrote url_shorten basic example

--- examples/basic/url_shorten.rb
+++ examples/basic/url_shorten.rb
@@ -4,22 +4,26 @@ require 'cinch'
 # Automatically shorten URL's found in messages
 # Using the tinyURL API
 
-bot = Cinch.configure do
-  server "irc.freenode.org"
-  verbose true
-  channels %w/#cinch/
-end
+bot = Cinch::Bot.new do
+  configure do |c|
+    c.server = "irc.freenode.org"
+  end
 
-def shorten(url)
-  url = open("http://tinyurl.com/api-create.php?url=#{URI.escape(url)}").read
-  url == "Error" ? nil : url
-rescue OpenURI::HTTPError
-  nil
-end
+  on :connect do
+    bot.join "#cinch"
+  end
 
-bot.on :privmsg do |m|
-  unless m.private?
-    urls = URI.extract(m.text, "http")
+  helpers do
+    def shorten(url)
+      url = open("http://tinyurl.com/api-create.php?url=#{URI.escape(url)}").read
+      url == "Error" ? nil : url
+    rescue OpenURI::HTTPError
+      nil
+    end
+  end
+
+  on :channel, /(.+)/ do |m, text|
+    urls = URI.extract(text, "http")
 
     unless urls.empty?
       short_urls = urls.map {|url| shorten(url) }.compact
