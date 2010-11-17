9280206fc68248daa2399bd05483ef01e9806926
Dominik Honnef <dominikh@fork-bomb.org>
2010-11-17 13:21:35 +0100

    add timer functionality to plugins

--- lib/cinch/plugin.rb
+++ lib/cinch/plugin.rb
@@ -5,6 +5,8 @@ module Cinch
     module ClassMethods
       Match = Struct.new(:pattern, :use_prefix, :method)
       Listener = Struct.new(:event, :method)
+      # @api private
+      Timer = Struct.new(:interval, :method, :threaded)
 
       # Set a match pattern.
       #
@@ -88,6 +90,21 @@ module Cinch
         @__cinch_name = name
       end
 
+      # @example
+      #   timer 5, method: :some_method
+      #   def some_method
+      #     Channel("#cinch-bots").send(Time.now.to_s)
+      #   end
+      # @param [Number] interval Interval in seconds
+      # @option options [Symbol] :method (:timer) Method to call
+      # @option options [Boolean] :threaded (true) Call method in a thread?
+      # @return [void]
+      def timer(interval, options = {})
+        options = {:method => :timer, :threaded => true}.merge(options)
+        @__cinch_timers ||= []
+        @__cinch_timers << Timer.new(interval, options[:method], options[:threaded])
+      end
+
       # @return [String]
       # @api private
       def __plugin_name
@@ -137,6 +154,34 @@ module Cinch
           bot.debug "[plugin] #{plugin_name}: Registering CTCP `#{ctcp}`"
           bot.on(:ctcp, ctcp, instance, ctcp) do |message, plugin, ctcp, *args|
             plugin.__send__("ctcp_#{ctcp.downcase}", message, *args)
+          end
+        end
+
+        (@__cinch_timers || []).each do |timer|
+          bot.debug "[plugin] #{__plugin_name}: Registering timer with interval `#{timer.interval}` for method `#{timer.method}`"
+          bot.on :connect do
+            Thread.new do
+              loop do
+                if instance.respond_to?(timer.method)
+                  l = lambda {
+                    begin
+                      instance.__send__(timer.method)
+                    rescue => e
+                      bot.logger.log_exception(e)
+                    end
+                  }
+
+                  if timer.threaded
+                    Thread.new do
+                      l.call
+                    end
+                  else
+                    l.call
+                  end
+                  sleep timer.interval
+                end
+              end
+            end
           end
         end
 
