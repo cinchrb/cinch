16bb96a4bfbc377fb51b64a779b1cd3ed3f88d5c
Dominik Honnef <dominikh@fork-bomb.org>
2010-11-17 03:21:45 +0100

    allow all on_* handlers in irc.rb to add additional information and/or events to the dispatch queue.
    Right now, no handler needs this functionality, but the upcoming
    addition of mode change specific events (and maybe more) will need
    this.

--- lib/cinch/irc.rb
+++ lib/cinch/irc.rb
@@ -71,35 +71,35 @@ module Cinch
       if ("001".."004").include? msg.command
         @registration << msg.command
         if registered?
-          events << :connect
+          events << [:connect]
         end
       elsif ["PRIVMSG", "NOTICE"].include?(msg.command)
-        events << :ctcp if msg.ctcp?
+        events << [:ctcp] if msg.ctcp?
         if msg.channel?
-          events << :channel
+          events << [:channel]
         else
-          events << :private
+          events << [:private]
         end
 
         if msg.command == "PRIVMSG"
-          events << :message
+          events << [:message]
         else
-          events << :notice
+          events << [:notice]
         end
       else
         meth = "on_#{msg.command.downcase}"
-        __send__(meth, msg) if respond_to?(meth, true)
+        __send__(meth, msg, events) if respond_to?(meth, true)
 
         if msg.error?
-          events << :error
+          events << [:error]
         else
-          events << msg.command.downcase.to_sym
+          events << [msg.command.downcase.to_sym]
         end
       end
 
-      msg.instance_variable_set(:@events, events)
-      events.each do |event|
-        @bot.dispatch(event, msg)
+      msg.instance_variable_set(:@events, events.map(&:first))
+      events.each do |event, *args|
+        @bot.dispatch(event, msg, *args)
       end
     end
 
@@ -115,7 +115,7 @@ module Cinch
     end
 
     private
-    def on_join(msg)
+    def on_join(msg, events)
       if msg.user == @bot
         @bot.channels << msg.channel
         msg.channel.sync_modes
@@ -123,7 +123,7 @@ module Cinch
       msg.channel.add_user(msg.user)
     end
 
-    def on_kick(msg)
+    def on_kick(msg, events)
       target = User.find_ensured(msg.params[1], @bot)
       if target == @bot
         @bot.channels.delete(msg.channel)
@@ -131,7 +131,7 @@ module Cinch
       msg.channel.remove_user(target)
     end
 
-    def on_kill(msg)
+    def on_kill(msg, events)
       user = User.find_ensured(msg.params[1], @bot)
       Channel.all.each do |channel|
         channel.remove_user(user)
@@ -139,7 +139,7 @@ module Cinch
       msg.user.unsync_all
     end
 
-    def on_mode(msg)
+    def on_mode(msg, events)
       if msg.channel?
         add_and_remove = @bot.irc.isupport["CHANMODES"]["A"] + @bot.irc.isupport["CHANMODES"]["B"] + @bot.irc.isupport["PREFIX"].keys
 
@@ -169,7 +169,7 @@ module Cinch
       end
     end
 
-    def on_nick(msg)
+    def on_nick(msg, events)
       if msg.user == @bot
         @bot.config.nick = msg.params.last
       end
@@ -178,40 +178,40 @@ module Cinch
       msg.user.instance_variable_set(:@nick, msg.params.last)
     end
 
-    def on_part(msg)
+    def on_part(msg, events)
       msg.channel.remove_user(msg.user)
       if msg.user == @bot
         @bot.channels.delete(msg.channel)
       end
     end
 
-    def on_ping(msg)
+    def on_ping(msg, events)
       message "PONG :#{msg.params.first}"
     end
 
-    def on_topic(msg)
+    def on_topic(msg, events)
       msg.channel.sync(:topic, msg.params[1])
     end
 
-    def on_quit(msg)
+    def on_quit(msg, events)
       Channel.all.each do |channel|
         channel.remove_user(msg.user)
       end
       msg.user.unsync_all
     end
 
-    def on_005(msg)
+    def on_005(msg, events)
       # ISUPPORT
       @isupport.parse(*msg.params[1..-2].map {|v| v.split(" ")}.flatten)
     end
 
-    def on_307(msg)
+    def on_307(msg, events)
       # RPL_WHOISREGNICK
       user = User.find_ensured(msg.params[1], @bot)
       @whois_updates[user].merge!({:authname => user.nick})
     end
 
-    def on_311(msg)
+    def on_311(msg, events)
       # RPL_WHOISUSER
       user = User.find_ensured(msg.params[1], @bot)
       @whois_updates[user].merge!({
@@ -221,7 +221,7 @@ module Cinch
                                   })
     end
 
-    def on_317(msg)
+    def on_317(msg, events)
       # RPL_WHOISIDLE
       user = User.find_ensured(msg.params[1], @bot)
       @whois_updates[user].merge!({
@@ -230,7 +230,7 @@ module Cinch
                                   })
     end
 
-    def on_318(msg)
+    def on_318(msg, events)
       # RPL_ENDOFWHOIS
       user = User.find_ensured(msg.params[1], @bot)
 
@@ -242,14 +242,14 @@ module Cinch
       end
     end
 
-    def on_319(msg)
+    def on_319(msg, events)
       # RPL_WHOISCHANNELS
       user = User.find_ensured(msg.params[1], @bot)
       channels = msg.params[2].scan(/#{@isupport["CHANTYPES"].join}[^ ]+/o).map {|c| Channel.find_ensured(c, @bot) }
       user.sync(:channels, channels, true)
     end
 
-    def on_324(msg)
+    def on_324(msg, events)
       # RPL_CHANNELMODEIS
 
       modes = {}
@@ -265,24 +265,24 @@ module Cinch
       msg.channel.sync(:modes, modes, false)
     end
 
-    def on_330(msg)
+    def on_330(msg, events)
       # RPL_WHOISACCOUNT
       user = User.find_ensured(msg.params[1], @bot)
       authname = msg.params[2]
       @whois_updates[user].merge!({:authname => authname})
     end
 
-    def on_331(msg)
+    def on_331(msg, events)
       # RPL_NOTOPIC
       msg.channel.sync(:topic, "")
     end
 
-    def on_332(msg)
+    def on_332(msg, events)
       # RPL_TOPIC
       msg.channel.sync(:topic, msg.params[2])
     end
 
-    def on_353(msg)
+    def on_353(msg, events)
       # RPL_NAMEREPLY
       unless @in_lists.include?(:names)
         msg.channel.clear_users
@@ -303,13 +303,13 @@ module Cinch
       end
     end
 
-    def on_366(msg)
+    def on_366(msg, events)
       # RPL_ENDOFNAMES
       @in_lists.delete :names
       msg.channel.mark_as_synced(:users)
     end
 
-    def on_367(msg)
+    def on_367(msg, events)
       # RPL_BANLIST
       unless @in_lists.include?(:bans)
         msg.channel.bans_unsynced.clear
@@ -324,7 +324,7 @@ module Cinch
       msg.channel.bans_unsynced << ban
     end
 
-    def on_368(msg)
+    def on_368(msg, events)
       # RPL_ENDOFBANLIST
       if @in_lists.include?(:bans)
         @in_lists.delete :bans
@@ -336,12 +336,12 @@ module Cinch
       msg.channel.mark_as_synced(:bans)
     end
 
-    def on_396(msg)
+    def on_396(msg, events)
       # note: designed for freenode
       User.find_ensured(msg.params[0], @bot).sync(:host, msg.params[1], true)
     end
 
-    def on_401(msg)
+    def on_401(msg, events)
       # ERR_NOSUCHNICK
       user = User.find_ensured(msg.params[1], @bot)
       user.sync(:unknown?, true, true)
@@ -351,7 +351,7 @@ module Cinch
       end
     end
 
-    def on_402(msg)
+    def on_402(msg, events)
       # ERR_NOSUCHSERVER
 
       if user = User.find(msg.params[1]) # not _ensured, we only want a user that already exists
@@ -361,12 +361,12 @@ module Cinch
       end
     end
 
-    def on_433(msg)
+    def on_433(msg, events)
       # ERR_NICKNAMEINUSE
       @bot.nick = msg.params[1] + "_"
     end
 
-    def on_671(msg)
+    def on_671(msg, events)
       user = User.find_ensured(msg.params[1], @bot)
       @whois_updates[user].merge!({:secure? => true})
     end
