e01ee6b40b4ccaeaf7c2cea4656543db7f95c6d4
Dominik Honnef <dominikh@fork-bomb.org>
2010-11-18 04:51:04 +0100

    add global riot helper test_user

--- test/setup.rb
+++ test/setup.rb
@@ -5,6 +5,12 @@ require 'riot/rr'
 require "cinch"
 
 Riot.verbose
+class Riot::Situation
+  def test_user(mask = "cinchy!cinch@cinchrb.org")
+    mock(test_user = Cinch::User.new("cinchy", nil)).mask { Cinch::Mask.new(mask) }
+    test_user
+  end
+end
 
 path = File.expand_path(File.dirname __FILE__)
 Dir["#{path}/cinch/**/*_test.rb"].each { |tf| require tf }
