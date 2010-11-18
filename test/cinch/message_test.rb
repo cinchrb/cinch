cad72f97a098f3d37ed681ff3bc81fe9e12b9c9b
Dominik Honnef <dominikh@fork-bomb.org>
2010-11-18 17:57:33 +0100

    add test-specific versions of Bot and IRC

--- test/cinch/message_test.rb
+++ test/cinch/message_test.rb
@@ -1,22 +1,20 @@
 context "A message" do
   context "coming from a server" do
-    setup { Cinch::Message.new(":localhost message", Cinch::Bot.new) }
+    setup { Cinch::Message.new(":localhost message", TestBot.new) }
     asserts(:server)
     asserts(:prefix).equals("localhost")
   end
 
   context "coming from a user" do
-    setup { Cinch::Message.new(":nick!user@host message", Cinch::Bot.new) }
+    setup { Cinch::Message.new(":nick!user@host message", TestBot.new) }
     asserts(:server).nil
     asserts(:prefix).equals("nick!user@host")
     asserts("User#nick") { topic.user.nick }.equals("nick")
-    asserts("User#user") { topic.user.user }.equals("user")
-    asserts("User#host") { topic.user.host }.equals("host")
     asserts("User") { topic.user.class }.equals(Cinch::User)
   end
 
   context "with a command" do
-    setup { Cinch::Message.new(":nick!user@host COMMAND", Cinch::Bot.new) }
+    setup { Cinch::Message.new(":nick!user@host COMMAND", TestBot.new) }
     asserts(:command).equals("COMMAND")
     asserts(:error?).equals(false)
     asserts(:error).nil
@@ -24,13 +22,13 @@ context "A message" do
     asserts(:ctcp?).equals(false)
 
     context "and arguments" do
-      setup { Cinch::Message.new(":nick!user@host COMMAND arg1 arg2", Cinch::Bot.new) }
+      setup { Cinch::Message.new(":nick!user@host COMMAND arg1 arg2", TestBot.new) }
       asserts(:params).equals(["arg1", "arg2"])
       asserts(:message).nil
       asserts(:ctcp?).equals(false)
 
       context "and a message" do
-        setup { Cinch::Message.new(":nick!user@host COMMAND arg1 arg2 :message with a : colon", Cinch::Bot.new) }
+        setup { Cinch::Message.new(":nick!user@host COMMAND arg1 arg2 :message with a : colon", TestBot.new) }
         asserts(:params).equals(["arg1", "arg2", "message with a : colon"])
         asserts(:message).equals("message with a : colon")
         asserts(:ctcp?).equals(false)
@@ -38,7 +36,7 @@ context "A message" do
     end
 
     context "and a message" do
-      setup { Cinch::Message.new(":nick!user@host COMMAND :message with a : colon", Cinch::Bot.new) }
+      setup { Cinch::Message.new(":nick!user@host COMMAND :message with a : colon", TestBot.new) }
       asserts(:params).equals(["message with a : colon"])
       asserts(:message).equals("message with a : colon")
       asserts(:ctcp?).equals(false)
@@ -46,7 +44,7 @@ context "A message" do
   end
 
   context "describing an error" do
-    setup { Cinch::Message.new(":localhost 433 * nick :Nickname is already in use.", Cinch::Bot.new) }
+    setup { Cinch::Message.new(":localhost 433 * nick :Nickname is already in use.", TestBot.new) }
     asserts(:numeric_reply?)
     asserts(:error?)
     asserts(:error).equals(433)
@@ -55,7 +53,7 @@ context "A message" do
   end
 
   context "describing a numeric reply" do
-    setup { Cinch::Message.new(":localhost 001 cinch :Welcome to the Internet Relay Chat Network cinch", Cinch::Bot.new) }
+    setup { Cinch::Message.new(":localhost 001 cinch :Welcome to the Internet Relay Chat Network cinch", TestBot.new) }
     asserts(:numeric_reply?)
     asserts(:command).equals("001")
     asserts(:params).equals(["cinch", "Welcome to the Internet Relay Chat Network cinch"])
@@ -63,7 +61,7 @@ context "A message" do
   end
 
   context "in a channel" do
-    setup { Cinch::Message.new(":nick!user@host PRIVMSG #channel :a message", Cinch::Bot.new) }
+    setup { Cinch::Message.new(":nick!user@host PRIVMSG #channel :a message", TestBot.new) }
     asserts(:channel?)
     asserts("Channel") { topic.channel.class }.equals(Cinch::Channel)
     asserts("Channel#name") { topic.channel.name }.equals("#channel")
@@ -71,20 +69,24 @@ context "A message" do
   end
 
   context "in private" do
-    setup { Cinch::Message.new(":nick!user@host PRIVMSG user :a message", Cinch::Bot.new) }
+    setup { Cinch::Message.new(":nick!user@host PRIVMSG user :a message", TestBot.new) }
     asserts(:channel?).equals(false)
     asserts(:ctcp?).equals(false)
   end
 
   context "describing a CTCP message" do
-    setup { Cinch::Message.new(":nick!user@host PRIVMSG cinch :\001PING\001", Cinch::Bot.new) }
+    setup { Cinch::Message.new(":nick!user@host PRIVMSG cinch :\001PING\001", TestBot.new) }
     asserts(:ctcp?)
     asserts(:ctcp_command).equals("PING")
     asserts(:ctcp_message).equals("PING")
     asserts(:ctcp_args).equals([])
+    asserts("can be replied to") {
+      topic.ctcp_reply("42")
+      topic.bot.raw_log.last == "NOTICE nick :\001PING 42\001"
+    }
 
     context "with parameters" do
-      setup { Cinch::Message.new(":nick!user@host NOTICE cinch :\001PING 123 456\001", Cinch::Bot.new) }
+      setup { Cinch::Message.new(":nick!user@host NOTICE cinch :\001PING 123 456\001", TestBot.new) }
       asserts(:ctcp?)
       asserts(:ctcp_command).equals("PING")
       asserts(:ctcp_message).equals("PING 123 456")
--- test/setup.rb
+++ test/setup.rb
@@ -20,5 +20,30 @@ class Riot::Situation
   end
 end
 
+
+class TestBot < Cinch::Bot
+  attr_reader :raw_log
+  def initialize(*args)
+    super
+    @irc = TestIRC.new
+    @raw_log = []
+  end
+
+  def raw(command)
+    @raw_log << command
+  end
+end
+
+class TestIRC
+  attr_reader :isupport
+  def initialize
+    @isupport = Cinch::ISupport.new
+  end
+end
+
+class Cinch::Message
+  attr_reader :bot
+end
+
 path = File.expand_path(File.dirname __FILE__)
 Dir["#{path}/cinch/**/*_test.rb"].each { |tf| require tf }
