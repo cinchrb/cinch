fc28f5c2e2b13a4a893d4b7c1d85758f7c84e3b1
Dominik Honnef <dominikh@fork-bomb.org>
2010-11-27 23:33:49 +0100

    fix bug in String#irc_{downcase,upcase} which caused Cinch to not properly replace a backslash

--- lib/cinch/rubyext/string.rb
+++ lib/cinch/rubyext/string.rb
@@ -2,9 +2,9 @@ class String
   def irc_downcase(mapping)
     case mapping
     when :rfc1459
-      self.tr("A-Z[]\\^", "a-z{}|~")
+      self.tr("A-Z[]\\\\^", "a-z{}|~")
     when :"strict-rfc1459"
-      self.tr("A-Z[]\\", "a-z{}|")
+      self.tr("A-Z[]\\\\", "a-z{}|")
     else
       # when :ascii or unknown/nil
       self.tr("A-Z", "a-z")
@@ -16,9 +16,9 @@ class String
     when :ascii
       self.tr("a-z", "A-Z")
     when :rfc1459
-      self.tr("a-z{}|~", "A-Z[]\\^")
+      self.tr("a-z{}|~", "A-Z[]\\\\^")
     when :strict-rfc1459
-      self.tr("a-z{}|", "A-Z[]\\")
+      self.tr("a-z{}|", "A-Z[]\\\\")
     end
   end
 end
