cinchrb/cinch

commit 260e9a6c52b9157a33148c2e6d14f4029a9eac7f

add timestamps to log messages in formatted logger

lib/cinch/logger/formatted_logger.rb
@@ -28,13 +28,13 @@ module Cinch
       def log(messages, kind = :generic)
         @mutex.synchronize do
           messages = [messages].flatten.map {|s| s.to_s.chomp}
-
-          messages.each do |message|
+          message = Time.now.strftime("[%Y/%m/%d %H:%M:%S.%L] ")
+          messages.each do |msg|
             if kind == :debug
               prefix = colorize("!! ", :yellow)
-              message = prefix + message
+              message << prefix + msg
             else
-              pre, msg = message.split(" :", 2)
+              pre, msg = msg.split(" :", 2)
               pre_parts = pre.split(" ")
 
               if kind == :incoming
@@ -52,7 +52,7 @@ module Cinch
                 pre_parts[0] = colorize(pre_parts[0], :bold)
               end
 
-              message = prefix + pre_parts.join(" ")
+              message << prefix + pre_parts.join(" ")
               message << colorize(" :#{msg}", :yellow) if msg
             end
             @output.puts message.encode

commit d4eabedac5e51fef1707eb8e1e4f9ea131c0c6d6

fix logging of multiple lines in FormattedLogger
this fixes a regression introduced by 260e9a6

lib/cinch/logger/formatted_logger.rb
@@ -28,8 +28,8 @@ module Cinch
       def log(messages, kind = :generic)
         @mutex.synchronize do
           messages = [messages].flatten.map {|s| s.to_s.chomp}
-          message = Time.now.strftime("[%Y/%m/%d %H:%M:%S.%L] ")
           messages.each do |msg|
+            message = Time.now.strftime("[%Y/%m/%d %H:%M:%S.%L] ")
             if kind == :debug
               prefix = colorize("!! ", :yellow)
               message << prefix + msg