5c6bcf300fd8a07ec496dc5f26ed5752f14cbb9a
Dominik Honnef <dominikh@fork-bomb.org>
2010-12-08 03:06:19 +0100

    fix handling of prefixes in Pattern.obj_to_r

--- lib/cinch/pattern.rb
+++ lib/cinch/pattern.rb
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 module Cinch
   class Pattern
-    def self.obj_to_r(obj, msg = nil, str_end_anchor = true)
+    def self.obj_to_r(obj, msg = nil, str_start_anchor = true, str_end_anchor = true)
       case obj
       when Regexp, NilClass
         return obj
       when Proc
-        return obj_to_r(obj.call(msg), msg, str_end_anchor)
+        return obj_to_r(obj.call(msg), msg, str_start_anchor, str_end_anchor)
       else
-        return /#{Regexp.escape(obj.to_s)}#{str_end_anchor ? "$" : ""}/
+        return /#{str_start_anchor ? "^" : ""}#{Regexp.escape(obj.to_s)}#{str_end_anchor ? "$" : ""}/
       end
     end
 
@@ -22,13 +22,22 @@ module Cinch
       prefix = Pattern.obj_to_r(@prefix, msg, true, false)
       end_anchor   = @pattern.is_a?(String) # important
 
-      pattern = Pattern.obj_to_r(@pattern, msg, end_anchor)
+      pattern = Pattern.obj_to_r(@pattern, msg, false, end_anchor)
       r = /#{prefix}#{pattern}/
       p r
       r
     end
   end
 end
+
+# on + regexp => regexp
+# on + string => ^string$
+
+# plugin + regexp => prefix + regexp + $
+# plugin + string => prefix + string + $
+# plugin + regexp + no_prefix => regexp
+# plugin + string + no_prefix => ^string$
+
 
 # [x] on·regexp = regexp [prefix=nil, pattern=regexp]
 # [x] plugin·regexp·no_prefix = regexp [prefix=nil, pattern=regexp]
