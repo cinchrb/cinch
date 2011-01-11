82239620d3fb0ab1f45c13310a69ecf3b0bc5643
Dominik Honnef <dominikh@fork-bomb.org>
2011-01-11 16:32:00 +0100

    add support for suffixes

--- lib/cinch/pattern.rb
+++ lib/cinch/pattern.rb
@@ -1,45 +1,45 @@
 # -*- coding: utf-8 -*-
 module Cinch
+  # @api private
   class Pattern
-    def self.obj_to_r(obj, msg = nil, str_start_anchor = true, str_end_anchor = true)
+    # @param [String, Regexp, NilClass, Proc, #to_s] obj The object to
+    #   convert to a regexp
+    # @return [Regexp, nil]
+    def self.obj_to_r(obj)
       case obj
       when Regexp, NilClass
         return obj
-      when Proc
-        return obj_to_r(obj.call(msg), msg, str_start_anchor, str_end_anchor)
       else
-        return /#{str_start_anchor ? "^" : ""}#{Regexp.escape(obj.to_s)}#{str_end_anchor ? "$" : ""}/
+        return Regexp.new(Regexp.escape(obj.to_s))
+      end
+    end
+
+    def self.resolve_proc(obj, msg = nil)
+      if obj.is_a?(Proc)
+        return resolve_proc(obj.call(msg), msg)
+      else
+        return obj
       end
     end
 
     attr_reader :prefix
+    attr_reader :suffix
     attr_reader :pattern
-    def initialize(prefix, pattern)
-      @prefix, @pattern = prefix, pattern
+    def initialize(prefix, pattern, suffix)
+      @prefix, @pattern, @suffix = prefix, pattern, suffix
     end
 
     def to_r(msg = nil)
-      prefix = Pattern.obj_to_r(@prefix, msg, true, false)
-      end_anchor   = @pattern.is_a?(String) # important
+      prefix  = Pattern.obj_to_r(Pattern.resolve_proc(@prefix, msg))
+      suffix  = Pattern.obj_to_r(Pattern.resolve_proc(@suffix, msg))
+      pattern = Pattern.resolve_proc(@pattern, msg)
 
-      pattern = Pattern.obj_to_r(@pattern, msg, false, end_anchor)
-      /#{prefix}#{pattern}/
+      case pattern
+      when Regexp, NilClass
+        /#{prefix}#{pattern}#{suffix}/
+      else
+        /^#{prefix}#{Pattern.obj_to_r(pattern)}#{suffix}$/
+      end
     end
   end
 end
-
-# on + regexp => regexp
-# on + string => ^string$
-
-# plugin + regexp => prefix + regexp + $
-# plugin + string => prefix + string + $
-# plugin + regexp + no_prefix => regexp
-# plugin + string + no_prefix => ^string$
-
-
-# [x] on·regexp = regexp [prefix=nil, pattern=regexp]
-# [x] plugin·regexp·no_prefix = regexp [prefix=nil, pattern=regexp]
-# [x] on·string = ^string$ [prefix=/^/, pattern=string]
-# [x] plugin·string·no_prefix = ^string$
-# [x] plugin·regexp·prefix = prefix + regexp [prefix=prefix, pattern=regexp] ← really?
-# [x] plugin·string·prefix = prefix + string + $ [prefix=prefix, pattern=string]
