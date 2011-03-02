cc5821e23b542acbf663f2cad4ce465e7874ef73
Dominik Honnef <dominikh@fork-bomb.org>
2011-03-02 20:38:26 +0100

    move implementation of helper methods (User, Channel) into the Helpers module

--- lib/cinch/bot.rb
+++ lib/cinch/bot.rb
@@ -34,6 +34,8 @@ require "cinch/user_manager"
 module Cinch
 
   class Bot
+    include Helpers
+
     # @return [Config]
     attr_reader :config
     # @return [IRC]
@@ -68,33 +70,6 @@ module Cinch
     attr_accessor :last_connection_was_successful
 
     # @group Helper methods
-
-    # Helper method for turning a String into a {Channel} object.
-    #
-    # @param [String] channel a channel name
-    # @return [Channel] a {Channel} object
-    # @example
-    #   on :message, /^please join (#.+)$/ do |m, target|
-    #     Channel(target).join
-    #   end
-    def Channel(channel)
-      return channel if channel.is_a?(Channel)
-      @channel_manager.find_ensured(channel)
-    end
-
-    # Helper method for turning a String into an {User} object.
-    #
-    # @param [String] user a user's nickname
-    # @return [User] an {User} object
-    # @example
-    #   on :message, /^tell me everything about (.+)$/ do |m, target|
-    #     user = User(target)
-    #     m.reply "%s is named %s and connects from %s" % [user.nick, user.name, user.host]
-    #   end
-    def User(user)
-      return user if user.is_a?(User)
-      @user_manager.find_ensured(user)
-    end
 
     # Define helper methods in the context of the bot.
     #
@@ -537,6 +512,10 @@ module Cinch
       end
 
       instance_eval(&b) if block_given?
+    end
+
+    def bot
+      self
     end
 
     # The bot's nickname.
