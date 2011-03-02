20cfec6d64da4766338ea8c2f97ca64d36227789
Dominik Honnef <dominikh@fork-bomb.org>
2011-03-02 16:03:37 +0100

    never return nil from Plugin#config

--- lib/cinch/plugin.rb
+++ lib/cinch/plugin.rb
@@ -295,7 +295,7 @@ module Cinch
     #
     # @return [Hash] A hash of options
     def config
-      @bot.config.plugins.options[self.class]
+      @bot.config.plugins.options[self.class] || {}
     end
 
     def self.included(by)
