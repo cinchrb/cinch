a32ac8d2113b955bc9dbcae01b89dbd6d7440b81
Dominik Honnef <dominikh@fork-bomb.org>
2011-05-25 18:10:34 +0200

    add documentation to the Timer class

--- lib/cinch/timer.rb
+++ lib/cinch/timer.rb
@@ -1,13 +1,24 @@
 module Cinch
   class Timer
+    # @return [Bot]
     attr_reader :bot
+    # @return [Number] The interval (in seconds) of the timer
     attr_accessor :interval
+    # @return [Boolean] If true, each invocation will be
+    #   executed in a thread of its own.
     attr_accessor :threaded
+    # @return [Proc]
     attr_reader :block
+    # @return [Boolean]
     attr_reader :started
     alias_method :threaded?, :threaded
     alias_method :started?, :started
 
+    # @param [Bot] bot The instance of {Bot} the timer is associated
+    #   with
+    # @param [Number] interval The interval (in seconds) of the timer
+    # @param [Boolean] threaded If true, each invocation will be
+    #   executed in a thread of its own.
     def initialize(bot, interval, threaded = true, &block)
       @bot      = bot
       @interval = interval
@@ -18,10 +29,14 @@ module Cinch
       @thread  = nil
     end
 
+    # @return [Boolean]
     def stopped?
       !@started
     end
 
+    # Start the timer
+    #
+    # @return [void]
     def start
       @thread = Thread.new do
         loop do
@@ -43,6 +58,9 @@ module Cinch
       @started = true
     end
 
+    # Stop the timer
+    #
+    # @return [void]
     def stop
       @thread.kill
       @started = false
