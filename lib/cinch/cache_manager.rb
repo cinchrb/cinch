89672504e570c0ae718cbe2faacc7c639f55c901
Dominik Honnef <dominikh@fork-bomb.org>
2011-05-26 00:37:11 +0200

    rename *Manager classes
    - rename CacheManager to CachedList
    - rename UserManager to UserList
    - rename ChannelManager to ChannelList
    
    Why? Because Manager sounds too much like Java. No kidding.

--- lib/cinch/cache_manager.rb
+++ lib/cinch/cache_manager.rb
@@ -1,16 +1,1 @@
-module Cinch
-  # @api private
-  class CacheManager
-    include Enumerable
-
-    def initialize(bot)
-      @bot = bot
-      @cache = {}
-      @mutex = Mutex.new
-    end
-
-    def each(&block)
-      @cache.each_value(&block)
-    end
-  end
-end
+require "cinch/cached_list"
---
+++ lib/cinch/channel_list.rb
@@ -0,0 +1,27 @@
+require "cinch/cache_manager"
+
+module Cinch
+  class ChannelList < CachedList
+    # Finds or creates a channel.
+    #
+    # @param [String] name name of a channel
+    # @return [Channel]
+    # @see Bot#Channel
+    def find_ensured(name)
+      downcased_name = name.irc_downcase(@bot.irc.isupport["CASEMAPPING"])
+      @mutex.synchronize do
+        @cache[downcased_name] ||= Channel.new(name, @bot)
+      end
+    end
+
+    # Finds a channel.
+    #
+    # @param [String] name name of a channel
+    # @return [Channel, nil]
+    def find(name)
+      downcased_name = name.irc_downcase(@bot.irc.isupport["CASEMAPPING"])
+      @cache[downcased_name]
+    end
+  end
+  ChannelManager = ChannelList
+end
