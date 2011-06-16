b16bdd1a066250ea08687917ad6655b9e3d59ce6
Dominik Honnef <dominikh@fork-bomb.org>
2011-06-16 18:35:28 +0200

    add deprecation warnings for deprecated files

--- lib/cinch/channel_manager.rb
+++ lib/cinch/channel_manager.rb
@@ -1,1 +1,2 @@
+$stderr.puts "cinch/channel_manager.rb will be removed in Cinch 2.0.0. Please use cinch/channel_list.rb instead."
 require "cinch/channel_list"
--- lib/cinch/user_manager.rb
+++ lib/cinch/user_manager.rb
@@ -1,1 +1,2 @@
+$stderr.puts "cinch/user_manager.rb will be removed in Cinch 2.0.0. Please use cinch/user_list.rb instead."
 require "cinch/user_list"
