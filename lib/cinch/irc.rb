72f944f8bf63c776f9b88ad016d7d5cb7a52d224
Dominik Honnef <dominikh@fork-bomb.org>
2011-06-30 02:02:38 +0200

    inform the user about excess floods and how to report bad throttling options

--- lib/cinch/irc.rb
+++ lib/cinch/irc.rb
@@ -315,6 +315,14 @@ module Cinch
       end
       msg.user.unsync_all
       @bot.user_manager.delete(msg.user)
+
+      if msg.message.downcase == "excess flood" && msg.user == @bot
+        @bot.debug ["Looks like your bot has been kicked because of excess flood.",
+                    "If you haven't modified the throttling options manually, please file a bug report at https://github.com/cinchrb/cinch/issues and include the following information:",
+                    "- Server: #{@bot.config.server}",
+                    "- Messages per second: #{@bot.config.messages_per_second}",
+                    "- Server queue size: #{@bot.config.server_queue_size}"]
+      end
     end
 
     def on_002(msg, events)
