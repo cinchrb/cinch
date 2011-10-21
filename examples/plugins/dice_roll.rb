e27af7deb573d544aa9e39b2c73fb0b529d1a17f
Dominik Honnef <dominikh@fork-bomb.org>
2011-10-21 17:53:47 +0200

    turn dice plugin into an actual dungeon dice roller

--- examples/plugins/dice_roll.rb
+++ examples/plugins/dice_roll.rb
@@ -1,22 +1,38 @@
 require 'cinch'
 
 class DiceRoll
-    include Cinch::Plugin
-    
-    match /roll/
-    
-    def execute(m)
-        roll = (Random.new).rand(1..6)
-        m.reply "Your dice roll was #{roll}", true
+  include Cinch::Plugin
+
+  # [[<repeats>#]<rolls>]d<sides>[<+/-><offset>]
+  match(/roll (?:(?:(\d+)#)?(\d+))?d(\d+)(?:([+-])(\d+))?/)
+  def execute(m, repeats, rolls, sides, offset_op, offset)
+    repeats = repeats.to_i
+    repeats = 1 if repeats < 1
+    rolls   = rolls.to_i
+    rolls   = 1 if rolls < 1
+
+    total = 0
+
+    repeats.times do
+      rolls.times do
+        score = rand(sides.to_i) + 1
+        if offset_op
+          score = score.send(offset_op, offset.to_i)
+        end
+        total += score
+      end
     end
+
+    m.reply "Your dice roll was: #{total}", true
+  end
 end
 
 bot = Cinch::Bot.new do
-    configure do |c|
-        c.server = 'irc.freenode.org'
-        c.channels = [ '#cinch-bots' ]
-        c.plugins.plugins = [ DiceRoll ]
-    end
+  configure do |c|
+    c.server = 'irc.freenode.org'
+    c.channels = ['#cinch-bots']
+    c.plugins.plugins = [DiceRoll]
+  end
 end
 
 bot.start
