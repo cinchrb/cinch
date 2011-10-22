0dc979ce667c7f5a23a7305ea2b08528f00d78da
Dominik Honnef <dominikh@fork-bomb.org>
2011-10-22 02:59:04 +0200

    fix string_to_const

--- lib/cinch/utilities/kernel.rb
+++ lib/cinch/utilities/kernel.rb
@@ -5,7 +5,7 @@ module Cinch
     module Kernel
       # @return [Object]
       def self.string_to_const(s)
-        return s unless s.is_a?(String)
+        return s unless s.is_a?(::String)
         s.split("::").inject(Kernel) {|base, name| base.const_get(name) }
       end
     end
