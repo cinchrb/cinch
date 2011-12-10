b6cde639cb50412ede95350184b1d3083a943717
Dominik Honnef <dominikh@fork-bomb.org>
2011-12-10 05:40:17 +0100

    use new storage API in memo example plugin

--- examples/plugins/memo.rb
+++ examples/plugins/memo.rb
@@ -1,39 +1,42 @@
 #!/usr/bin/env ruby
 
 require 'cinch'
+require 'cinch/storage/yaml'
 
 class Memo
-  class MemoStruct < Struct.new(:nick, :channel, :text, :time)
+  class MemoStruct < Struct.new(:user, :channel, :text, :time)
     def to_s
-      "[#{time.asctime}] <#{channel}/#{nick}> #{text}"
+      "[#{time.asctime}] <#{channel}/#{user}> #{text}"
     end
   end
 
   include Cinch::Plugin
 
+  def initialize(*args)
+    super
+    storage[:memos] = {}
+  end
+
   listen_to :message
   match /memo (.+?) (.+)/
 
-  def initialize(*args)
-    super
-    @memos = {}
-  end
-
   def listen(m)
-    if @memos.has_key?(m.user.nick)
-      m.user.send @memos.delete(m.user.nick).to_s
+    if storage[:memos].has_key?(m.user.nick)
+      m.user.send storage[:memos].delete(m.user.nick).to_s
+      storage.save
     end
   end
 
   def execute(m, nick, message)
-    if @memos.key?(nick)
+    if storage[:memos].key?(nick)
       m.reply "There's already a memo for #{nick}. You can only store one right now"
     elsif nick == m.user.nick
       m.reply "You can't leave memos for yourself.."
     elsif nick == bot.nick
       m.reply "You can't leave memos for me.."
     else
-      @memos[nick] = MemoStruct.new(m.user.nick, m.channel, message, Time.now)
+      storage[:memos][nick] = MemoStruct.new(m.user.name, m.channel.name, message, Time.now)
+      storage.save
       m.reply "Added memo for #{nick}"
     end
   end
@@ -44,6 +47,9 @@ bot = Cinch::Bot.new do
     c.server = "irc.freenode.org"
     c.channels = ["#cinch-bots"]
     c.plugins.plugins = [Memo]
+    c.storage.backend = Cinch::Storage::YAML
+    c.storage.basedir = "./yaml/"
+    c.storage.autosave = true
   end
 end
 
