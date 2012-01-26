70870a9189e1556007802edbd7dc06792eeebf2e
Dominik Honnef <dominikh@fork-bomb.org>
2012-01-26 11:31:33 +0100

    fix documentation of LoggerList

--- lib/cinch/logger_list.rb
+++ lib/cinch/logger_list.rb
@@ -5,6 +5,8 @@ module Cinch
   #
   # It inherits directly from Array, so adding new loggers is as easy
   # as calling LoggerList#push.
+  #
+  # @attr_writer level
   # @since 2.0.0
   class LoggerList < Array
     # (see Logger#level=)
@@ -12,47 +14,47 @@ module Cinch
       each {|l| l.level = level}
     end
 
-    # (see Logger::Logger#log)
+    # (see Logger#log)
     def log(messages, event = :debug, level = event)
       each {|l| l.log(messages, event, level)}
     end
 
-    # (see Logger::Logger#debug)
+    # (see Logger#debug)
     def debug(message)
       each {|l| l.debug(message)}
     end
 
-    # (see Logger::Logger#error)
+    # (see Logger#error)
     def error(message)
       each {|l| l.error(message)}
     end
 
-    # (see Logger::Logger#error)
+    # (see Logger#error)
     def fatal(message)
       each {|l| l.fatal(message)}
     end
 
-    # (see Logger::Logger#info)
+    # (see Logger#info)
     def info(message)
       each {|l| l.info(message)}
     end
 
-    # (see Logger::Logger#warn)
+    # (see Logger#warn)
     def warn(message)
       each {|l| l.warn(message)}
     end
 
-    # (see Logger::Logger#incoming)
+    # (see Logger#incoming)
     def incoming(message)
       each {|l| l.incoming(message)}
     end
 
-    # (see Logger::Logger#outgoing)
+    # (see Logger#outgoing)
     def outgoing(message)
       each {|l| l.outgoing(message)}
     end
 
-    # (see Logger::Logger#exception)
+    # (see Logger#exception)
     def exception(message)
       each {|l| l.exception(message)}
     end
