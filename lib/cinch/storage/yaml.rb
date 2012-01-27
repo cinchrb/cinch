05dea4d439a5c38e9e2e1fa2e28e8cc4c33ff8fb
Dominik Honnef <dominikh@fork-bomb.org>
2012-01-28 00:01:32 +0100

    document YAML storage

--- lib/cinch/storage/yaml.rb
+++ lib/cinch/storage/yaml.rb
@@ -3,7 +3,9 @@ require "yaml"
 
 module Cinch
   class Storage
+    # A basic storage backed by YAML, using one file per plugin.
     class YAML < Storage
+      # (see Storage#initialize)
       def initialize(options, plugin)
         # We are a basic example, so we load everything into memory. yey.
         @file = options.basedir + plugin.class.plugin_name + ".yaml"
@@ -17,14 +19,17 @@ module Cinch
         @mutex = Mutex.new
       end
 
+      # (see Storage#[])
       def [](key)
         @yaml[key]
       end
 
+      # (see Strage#[]=)
       def []=(key, value)
         @yaml[key] = value
       end
 
+      # (see Storage#has_key?)
       def has_key?(key)
         @yaml.has_key?(key)
       end
@@ -32,30 +37,35 @@ module Cinch
       alias_method :key?, :has_key?
       alias_method :member?, :has_key?
 
+      # (see Storage#each)
       def each
         @yaml.each {|e| yield(e)}
 
         self
       end
 
+      # (see Storage#each_key)
       def each_key
         @yaml.each_key {|e| yield(e)}
 
         self
       end
 
+      # (see Storage#each_value)
       def each_value
         @yaml.each_value {|e| yield(e)}
 
         self
       end
 
+      # (see Storage#delete)
       def delete(key)
         obj = @yaml.delete(key)
 
         obj
       end
 
+      # (see Storage#delete_if)
       def delete_if
         delete_keys = []
         each do |key, value|
@@ -69,6 +79,7 @@ module Cinch
         self
       end
 
+      # (see Storage#save)
       def save
         @mutex.synchronize do
           File.open(@file, "w") do |f|
@@ -77,6 +88,7 @@ module Cinch
         end
       end
 
+      # (see Storage#unload)
       def unload
       end
     end
