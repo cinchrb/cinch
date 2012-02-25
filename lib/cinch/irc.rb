cinchrb/cinch

commit 1ce8b2210e706290e9a382396ffbe14c24622e23

replace IRCd class with Network class and adjust detection to detect both network and ircd

lib/cinch/irc.rb
@@ -1,6 +1,6 @@
 require "timeout"
 require "net/protocol"
-require "cinch/ircd"
+require "cinch/network"
 
 module Cinch
   # This class manages the connection to the IRC server. That includes
@@ -15,8 +15,8 @@ module Cinch
     # @return [Bot]
     attr_reader :bot
 
-    # @return [IRCd] The detected IRCd
-    attr_reader :ircd
+    # @return [Network] The detected network
+    attr_reader :network
 
     def initialize(bot)
       @bot      = bot
@@ -28,7 +28,7 @@ module Cinch
     # @since 2.0.0
     def setup
       @registration  = []
-      @ircd          = IRCd.new(:unknown)
+      @network       = Network.new(:unknown, :unknown)
       @whois_updates = Hash.new {|h, k| h[k] = {}}
       @in_lists      = Set.new
     end
@@ -244,9 +244,10 @@ module Cinch
     end
 
     # @since 2.0.0
-    def detect_ircd(msg, event)
-      old_ircd = @ircd
-      new_ircd = nil
+    def detect_network(msg, event)
+      old_network = @network
+      new_network = nil
+      new_ircd    = nil
       case event
       when "002"
         if msg.params.last =~ /^Your host is .+?, running version (.+)$/
@@ -259,21 +260,37 @@ module Cinch
             new_ircd = $1.downcase.to_sym
           end
         elsif msg.params.last == "Your host is jtvchat"
-          new_ircd = :jtv
+          new_network = :jtv
+          new_ircd    = :jtv
         end
       when "005"
-        if @isupport["NETWORK"] == "NGameTV"
-          new_ircd = :ngametv
+        case @isupport["NETWORK"]
+        when "NGameTV"
+          new_network = :ngametv
+          new_ircd    = :ngametv
+        when nil
+        else
+          new_network = @isupport["NETWORK"].downcase.to_sym
         end
       end
 
-      if old_ircd.unknown? && new_ircd
+      new_network ||= old_network.name
+      new_ircd    ||= old_network.ircd
+
+      if old_network.unknown_ircd? && new_ircd != :unknown
         @bot.loggers.info "Detected IRCd: #{new_ircd}"
-        @ircd = IRCd.new(new_ircd)
-      elsif !old_ircd.unknown? && new_ircd && new_ircd != old_ircd.name
-        @bot.loggers.info "Detected different IRCd: #{old_ircd.name} -> #{new_ircd}"
-        @ircd = IRCd.new(new_ircd)
       end
+      if !old_network.unknown_ircd? && new_ircd != old_network.ircd
+        @bot.loggers.info "Detected different IRCd: #{old_network.ircd} -> #{new_ircd}"
+      end
+      if old_network.unknown_network? && new_network != :unknown
+        @bot.loggers.info "Detected network: #{new_network}"
+      end
+      if !old_network.unknown_network? && new_network != old_network.name
+        @bot.loggers.info "Detected different network: #{old_network.name} -> #{new_network}"
+      end
+
+      @network = Network.new(new_network, new_ircd)
     end
 
     def process_owner_mode(msg, events, param, direction)
@@ -453,13 +470,13 @@ module Cinch
 
     # @since 2.0.0
     def on_002(msg, events)
-      detect_ircd(msg, "002")
+      detect_network(msg, "002")
     end
 
     def on_005(msg, events)
       # ISUPPORT
       @isupport.parse(*msg.params[1..-2].map {|v| v.split(" ")}.flatten)
-      detect_ircd(msg, "005")
+      detect_network(msg, "005")
     end
 
     # @since 1.1.0

lib/cinch/channel.rb
@@ -69,8 +69,8 @@ module Cinch
           when :bans
             @bot.irc.send "MODE #@name +b"
           when :owners
-            if @bot.irc.ircd.owner_list_mode
-              @bot.irc.send "MODE #@name +#{@bot.irc.ircd.owner_list_mode}"
+            if @bot.irc.network.owner_list_mode
+              @bot.irc.send "MODE #@name +#{@bot.irc.network.owner_list_mode}"
             else
               # the current IRCd does not support channel owners, so
               # just mark the empty array as synced
@@ -218,8 +218,8 @@ module Cinch
       @bot.irc.send "NAMES #@name" if all
       @bot.irc.send "MODE #@name +b" # bans
       @bot.irc.send "MODE #@name"
-      if @bot.irc.ircd.owner_list_mode
-        @bot.irc.send "MODE #@name +#{@bot.irc.ircd.owner_list_mode}"
+      if @bot.irc.network.owner_list_mode
+        @bot.irc.send "MODE #@name +#{@bot.irc.network.owner_list_mode}"
       else
         mark_as_synced :owners
       end

lib/cinch/message.rb
@@ -60,7 +60,7 @@ module Cinch
       match = @raw.match(/(^:(\S+) )?(\S+)(.*)/)
       _, @prefix, @command, raw_params = match.captures
 
-      if @bot.irc.ircd.ngametv?
+      if @bot.irc.network.ngametv?
         if @prefix != "ngame"
           @prefix = "%s!user@host" % [@prefix, @prefix, @prefix]
         end

lib/cinch/network.rb
@@ -1,25 +1,29 @@
 module Cinch
   # This class allows querying the IRC server for its name and certain
   # non-standard behaviour.
-  class IRCd
+  class Network
     # @return [Symbol]
     attr_reader :name
 
+    # @return [Symbol]
+    attr_reader :ircd
+
     # @param [Symbol] name
-    def initialize(name)
+    def initialize(name, ircd)
       @name = name
+      @ircd = ircd
     end
 
     # @return [String, nil] The mode used for getting the list of
     #   channel owners, if any
     def owner_list_mode
-      return "q" if @name == :unreal
+      return "q" if @ircd == :unreal
     end
 
     # @return [String, nil] The mode used for getting the list of
     #   channel quiets, if any
     def quiet_list_mode
-      return "q" if @name == :"ircd-seven"
+      return "q" if @ircd == :"ircd-seven"
     end
 
     # @return [Boolean] Does WHOIS only support one argument?
@@ -38,9 +42,19 @@ module Cinch
     end
 
     # @return [Boolean] True if we do not know which network we are
-    #   connected to.
-    def unknown?
+    #   connected to
+    def unknown_network?
       @name == :unknown
+    end
+
+    # @return [Boolean] True if we do not know which software the
+    #   server is running
+    def unknown_ircd?
+      @ircd == :unknown
+    end
+
+    def default_messages_per_second
+
     end
   end
 end

lib/cinch/user.rb
@@ -144,7 +144,7 @@ module Cinch
       end
 
       @in_whois = true
-      if @bot.irc.ircd.whois_only_one_argument?
+      if @bot.irc.network.whois_only_one_argument?
         @bot.irc.send "WHOIS #@name"
       else
         @bot.irc.send "WHOIS #@name #@name"

commit dbba7d240183458fc4a11217db33c0b7f3f0637e

in irc.rb: replace remaining @ircd by @network
fixes regression introduced by 1ce8b2210e706290e9a382396ffbe14c24622e23

lib/cinch/irc.rb
@@ -381,7 +381,7 @@ module Cinch
                 events << [:unban, ban]
               end
             when "q"
-              process_owner_mode(msg, events, param, direction) if @ircd.owner_list_mode
+              process_owner_mode(msg, events, param, direction) if @network.owner_list_mode
             else
               raise Exceptions::UnsupportedMode, mode
             end
@@ -596,7 +596,7 @@ module Cinch
       @in_lists << :bans
 
       mask = msg.params[2]
-      if @ircd.jtv?
+      if @network.jtv?
         # on the justin tv network, ban "masks" only consist of the
         # nick/username
         mask = "%s!%s@%s" % [mask, mask, mask + ".irc.justin.tv"]