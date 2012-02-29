e6b89bec75bac54827cd8e19bf1d774ddecda8fa
Dominik Honnef <dominikh@fork-bomb.org>
2012-02-29 19:12:52 +0100

    add api documentation to sasl classes

--- lib/cinch/sasl.rb
+++ lib/cinch/sasl.rb
@@ -1,3 +1,13 @@
 require "cinch/sasl/diffie_hellman"
 require "cinch/sasl/plain"
 require "cinch/sasl/dh_blowfish"
+
+module Cinch
+  # @note All classes and modules in this module are for internal use by
+  #   Cinch only.
+  #
+  # @api private
+  # @since 2.0.0
+  module SASL
+  end
+end
--- lib/cinch/sasl/plain.rb
+++ lib/cinch/sasl/plain.rb
@@ -5,6 +5,9 @@ module Cinch
   module SASL
     class Plain < Mechanism
       class << self
+        # @param [String] user
+        # @param [String] password
+        # @return [String]
         def generate(user, password, _ = nil)
           Base64.strict_encode64([user, user, password].join("\0"))
         end
