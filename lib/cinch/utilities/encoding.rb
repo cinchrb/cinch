d92850456b7cc4ecda33067845ef2df43207f428
Konrad Borowski <x.fix@o2.pl>
2014-09-11 18:48:54 +0200

    Always use UTF-8 for output with IRC encoding.

--- lib/cinch/utilities/encoding.rb
+++ lib/cinch/utilities/encoding.rb
@@ -11,11 +11,6 @@ module Cinch
           # is performed. This allows you to see non-ASCII from mIRC
           # users (non-UTF-8) and other users sending you UTF-8.
           #
-          # CP1252 encoding is compatible with ISO-8859-1 encoding. The
-          # reason for choice of it instead of ISO-8859-1 is that some
-          # IRC clients may send CP1252 messages. Cinch will never send them,
-          # but it's possible that others will actually do.
-          #
           # (from http://xchat.org/encoding/#hybrid)
           string.force_encoding("UTF-8")
           if !string.valid_encoding?
@@ -32,26 +27,10 @@ module Cinch
       def self.encode_outgoing(string, encoding)
         string = string.dup
         if encoding == :irc
-          # If your text contains only characters that fit inside the ISO-8859-1
-          # code page (aka Latin1), the entire line will be sent that way. mIRC
-          # users should see it correctly. XChat users who are using UTF-8 will
-          # also see it correctly, because it will fail UTF-8 validation and will
-          # be assumed to be ISO-8859-1, even by older XChat versions.
-          #
-          # If the text doesn't fit inside the ISO-8859-1 code page, (for example if you
-          # type Eastern European characters, or Russian) it will be sent as UTF-8. Only
-          # UTF-8 capable clients will be able to see these characters correctly
-          #
-          # (from http://xchat.org/encoding/#hybrid)
-          begin
-            string.encode!("ISO-8859-1")
-          rescue ::Encoding::UndefinedConversionError
-          end
-        else
-          string.encode!(encoding, {:invalid => :replace, :undef => :replace}).force_encoding("ASCII-8BIT")
+          encoding = "UTF-8"
         end
 
-        return string
+        return string.encode!(encoding, {:invalid => :replace, :undef => :replace}).force_encoding("ASCII-8BIT")
       end
     end
   end
