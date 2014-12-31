cinchrb/cinch

commit e17f84567afbb453a52f04f07c8373af625018dd

fix whois updates in NOSUCHNICK cases

lib/cinch/irc.rb
@@ -36,7 +36,7 @@ module Cinch
     def setup
       @registration  = []
       @network       = Network.new(:unknown, :unknown)
-      @whois_updates = Hash.new {|h, k| h[k] = {}}
+      @whois_updates = {}
       @in_lists      = Set.new
     end
 
@@ -366,6 +366,11 @@ module Cinch
       end
     end
 
+    def update_whois(user, data)
+      @whois_updates[user] ||= {}
+      @whois_updates[user].merge!(data)
+    end
+
     # @since 2.0.0
     def on_away(msg, events)
       if msg.message.to_s.empty?
@@ -603,7 +608,7 @@ module Cinch
       away = msg.message
 
       if @whois_updates[user]
-        @whois_updates[user].merge!({:away => away})
+        update_whois(user, {:away => away})
       end
     end
 
@@ -611,36 +616,38 @@ module Cinch
     def on_307(msg, events)
       # RPL_WHOISREGNICK
       user = User(msg.params[1])
-      @whois_updates[user].merge!({:authname => user.nick})
+      update_whois(user, {:authname => user.nick})
     end
 
     def on_311(msg, events)
       # RPL_WHOISUSER
       user = User(msg.params[1])
-      @whois_updates[user].merge!({
-                                    :user => msg.params[2],
-                                    :host => msg.params[3],
-                                    :realname => msg.params[5],
-                                  })
+      update_whois(user, {
+                     :user => msg.params[2],
+                     :host => msg.params[3],
+                     :realname => msg.params[5],
+                   })
     end
 
     def on_317(msg, events)
       # RPL_WHOISIDLE
       user = User(msg.params[1])
-      @whois_updates[user].merge!({
-                                    :idle => msg.params[2].to_i,
-                                    :signed_on_at => Time.at(msg.params[3].to_i),
-                                  })
+      update_whois(user, {
+                     :idle => msg.params[2].to_i,
+                     :signed_on_at => Time.at(msg.params[3].to_i),
+                   })
     end
 
     def on_318(msg, events)
       # RPL_ENDOFWHOIS
       user = User(msg.params[1])
 
-      if @whois_updates[user].empty? && !user.attr(:unknown?, true, true)
-        user.end_of_whois(nil)
-      else
-        user.end_of_whois(@whois_updates[user])
+      if @whois_updates[user]
+        if @whois_updates[user].empty? && !user.attr(:unknown?, true, true)
+          user.end_of_whois(nil)
+        else
+          user.end_of_whois(@whois_updates[user])
+        end
         @whois_updates.delete user
       end
     end
@@ -815,10 +822,7 @@ module Cinch
 
     def on_401(msg, events)
       # ERR_NOSUCHNICK
-      user = User(msg.params[1])
-      user.sync(:unknown?, true, true)
-      msg.user.online = false
-      if @whois_updates.key?(user)
+      if user = @bot.user_list.find(msg.params[1])
         user.end_of_whois(nil, true)
         @whois_updates.delete user
       end

commit 4bdc470d37be24be0ea8f00801f600df42aa834f

Handle rate-limited WHOIS
Some networks, such as freenode, sometimes send a 263 + end of whois
without any actual data. In the past, we handled that case correctly,
and retried the WHOIS, until we broke it in
e17f84567afbb453a52f04f07c8373af625018dd.

Closes gh-183

lib/cinch/irc.rb
@@ -648,14 +648,14 @@ module Cinch
       # RPL_ENDOFWHOIS
       user = User(msg.params[1])
 
-      if @whois_updates[user]
-        if @whois_updates[user].empty? && !user.attr(:unknown?, true, true)
-          user.end_of_whois(nil)
-        else
-          user.end_of_whois(@whois_updates[user])
-        end
-        @whois_updates.delete user
+      if @whois_updates[user].nil? ||
+         (@whois_updates[user].empty? && !user.attr(:unknown?, true, true))
+        user.end_of_whois(nil)
+        return
       end
+
+      user.end_of_whois(@whois_updates[user])
+      @whois_updates.delete user
     end
 
     def on_319(msg, events)