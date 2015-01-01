590aa3f30f998904e9f508ebb5875e96600f679a
Andrew Kreps <andrew.kreps@gmail.com>
2014-12-31 18:02:44 -0800

    fix string interpolation in deprecation warning

--- lib/cinch/utilities/deprecation.rb
+++ lib/cinch/utilities/deprecation.rb
@@ -6,7 +6,7 @@ module Cinch
       def self.print_deprecation(version, method, instead = nil)
         s = "Deprecation warning: Beginning with version #{version}, #{method} should not be used anymore."
         if instead != nil
-          s << " Use ${instead} instead."
+          s << " Use #{instead} instead."
         end
         $stderr.puts s
         $stderr.puts caller
