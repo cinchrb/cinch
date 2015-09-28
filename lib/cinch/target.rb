cinchrb/cinch

commit 3dc0f86385755078200b0c9ab764901fa42d3260

Fix calculation of message split position
acc_rune_sizes should be set for each split because max_rune changes
when single-byte and multi-byte characters are in the chunks.

r should be (max_rune + 1) in slicing msg in 0...r (**less** than r).
max_rune is a zero-based index.

Additionally, use \z instead of \u00A0 to prevent word-splitting, as \z
only takes up a single byte, same as an actual space.

lib/cinch/target.rb
@@ -42,7 +42,6 @@ module Cinch
         splitted = split_message(line, prefix, split_start, split_end)
 
         splitted[0, (@bot.config.max_messages || splitted.size)].each do |string|
-          string.tr!("\u00A0", " ") # clean string from any non-breaking spaces
           @bot.irc.send("#{command} #@name :#{string}")
         end
       end
@@ -178,17 +177,22 @@ module Cinch
       end
 
       splitted = []
-      acc = 0
-      acc_rune_sizes = msg.each_char.map {|ch|
-        acc += ch.bytesize
-      }
       while msg.bytesize > max_bytesize_without_end
+        acc = 0
+        acc_rune_sizes = msg.each_char.map {|ch|
+          acc += ch.bytesize
+        }
+
         max_rune = acc_rune_sizes.rindex {|bs| bs <= max_bytesize_without_end} || 0
-        r = [msg.rindex(/\s/, max_rune) || max_rune, 1].max
-        splitted << (msg[0...r] + split_end.tr(" ", "\u00A0"))
-        msg = split_start.tr(" ", "\u00A0") + msg[r..-1].lstrip
+        r = [msg.rindex(/\s/, max_rune) || (max_rune + 1), 1].max
+
+        splitted << (msg[0...r] + split_end)
+        msg = split_start.tr(" ", "\z") + msg[r..-1].lstrip
       end
       splitted << msg
+
+      # clean string from any substitute characters
+      splitted.map {|string| string.tr("\z", " ")}
     end
   end
 end

commit 3fe2a9f52a27f57c3d732d29151b6e331974d438

Use correct escape sequence in split_message
The escape sequence "\z" doesn't actually exist and evaluates to a plain
'z'. We meant to use "\cz". This is a regression introduced in
3dc0f86385755078200b0c9ab764901fa42d3260.

lib/cinch/target.rb
@@ -187,12 +187,12 @@ module Cinch
         r = [msg.rindex(/\s/, max_rune) || (max_rune + 1), 1].max
 
         splitted << (msg[0...r] + split_end)
-        msg = split_start.tr(" ", "\z") + msg[r..-1].lstrip
+        msg = split_start.tr(" ", "\cz") + msg[r..-1].lstrip
       end
       splitted << msg
 
       # clean string from any substitute characters
-      splitted.map {|string| string.tr("\z", " ")}
+      splitted.map {|string| string.tr("\cz", " ")}
     end
   end
 end