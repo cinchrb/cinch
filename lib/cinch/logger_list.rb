cinchrb/cinch

commit d0ede97dda0677ddd756b7bec03f5e1f0681aba6

Implement log filtering
Log filters take log messages as input and return modified messages.
Additionally, they can drop messages by returning nil.

This allows for filtering passwords or other information from log
messages.

Closes gh-194

lib/cinch/logger_list.rb
@@ -9,6 +9,17 @@ module Cinch
   # @attr_writer level
   # @since 2.0.0
   class LoggerList < Array
+    # A list of log filters that will be applied before emitting a log
+    # message.
+    #
+    # @return [Array<LogFilter>]
+    # @since 2.3.0
+    attr_accessor :filters
+    def initialize(*args)
+      @filters = []
+      super
+    end
+
     # (see Logger#level=)
     def level=(level)
       each {|l| l.level = level}
@@ -16,47 +27,64 @@ module Cinch
 
     # (see Logger#log)
     def log(messages, event = :debug, level = event)
-      each {|l| l.log(messages, event, level)}
+      do_log(messages, event, level)
     end
 
     # (see Logger#debug)
     def debug(message)
-      each {|l| l.debug(message)}
+      do_log(message, :debug)
     end
 
     # (see Logger#error)
     def error(message)
-      each {|l| l.error(message)}
+      do_log(message, :error)
     end
 
     # (see Logger#error)
     def fatal(message)
-      each {|l| l.fatal(message)}
+      do_log(message, :fatal)
     end
 
     # (see Logger#info)
     def info(message)
-      each {|l| l.info(message)}
+      do_log(message, :info)
     end
 
     # (see Logger#warn)
     def warn(message)
-      each {|l| l.warn(message)}
+      do_log(message, :warn)
     end
 
     # (see Logger#incoming)
     def incoming(message)
-      each {|l| l.incoming(message)}
+      do_log(message, :incoming, :log)
     end
 
     # (see Logger#outgoing)
     def outgoing(message)
-      each {|l| l.outgoing(message)}
+      do_log(message, :outgoing, :log)
     end
 
     # (see Logger#exception)
     def exception(e)
-      each {|l| l.exception(e)}
+      do_log(e, :exception, :error)
+    end
+
+    private
+    def do_log(messages, event, level = event)
+      messages = Array(messages)
+      if event != :exception
+        messages.map! { |m|
+          @filters.each do |f|
+            m = f.filter(m, event)
+            if m.nil?
+              break
+            end
+          end
+          m
+        }.compact
+      end
+      each {|l| l.log(messages, event, level)}
     end
   end
 end

lib/cinch/log_filter.rb
@@ -1,0 +1,21 @@
+module Cinch
+  # LogFilter describes an interface for filtering log messages before
+  # they're printed.
+  #
+  # @abstract
+  # @since 2.3.0
+  class LogFilter
+    # filter is called for each log message, except for exceptions. It
+    # returns a new string, which is the one that should be printed, or
+    # further filtered by other filters. Returning nil will drop the
+    # message.
+    #
+    # @param [String] message The message that is to be logged
+    # @param [:debug, :incoming, :outgoing, :info, :warn, :exception,
+    #   :error, :fatal] event The kind of message
+    # @return [String, nil] The modified message, as it should be
+    #   logged, or nil if the message shouldn't be logged at all
+    def filter(message, event)
+    end
+  end
+end

commit b310ede2e902a891346c3e91a5f7e50aca03ee2f

Restore old logging behaviour
d0ede97 broke the way we were doing logging. Previously, a call to
LoggerList#method would correspond to calls to Logger#method – after the
change, they resulted in Logger#log calls only. This was made under the
assumption that loggers only overwrote the format_* methods – However,
they're also free to overwrite the incoming, outgoing, exception, ...
methods.

This change restores the old behaviour by calling these methods again.

lib/cinch/logger_list.rb
@@ -27,64 +27,59 @@ module Cinch
 
     # (see Logger#log)
     def log(messages, event = :debug, level = event)
-      do_log(messages, event, level)
+      messages = Array(messages).map {|m| filter(m, event)}.compact
+      each {|l| l.log(messages, event, level)}
     end
 
     # (see Logger#debug)
     def debug(message)
-      do_log(message, :debug)
+      (m = filter(message, :debug)) && each {|l| l.debug(m)}
     end
 
     # (see Logger#error)
     def error(message)
-      do_log(message, :error)
+      (m = filter(message, :error)) && each {|l| l.error(m)}
     end
 
     # (see Logger#error)
     def fatal(message)
-      do_log(message, :fatal)
+      (m = filter(message, :fatal)) && each {|l| l.fatal(m)}
     end
 
     # (see Logger#info)
     def info(message)
-      do_log(message, :info)
+      (m = filter(message, :info)) && each {|l| l.info(m)}
     end
 
     # (see Logger#warn)
     def warn(message)
-      do_log(message, :warn)
+      (m = filter(message, :warn)) && each {|l| l.warn(m)}
     end
 
     # (see Logger#incoming)
     def incoming(message)
-      do_log(message, :incoming, :log)
+      (m = filter(message, :incoming)) && each {|l| l.incoming(m)}
     end
 
     # (see Logger#outgoing)
     def outgoing(message)
-      do_log(message, :outgoing, :log)
+      (m = filter(message, :outgoing)) && each {|l| l.outgoing(m)}
     end
 
     # (see Logger#exception)
     def exception(e)
-      do_log(e, :exception, :error)
+      each {|l| l.exception(e)}
     end
 
     private
-    def do_log(messages, event, level = event)
-      messages = Array(messages)
-      if event != :exception
-        messages.map! { |m|
-          @filters.each do |f|
-            m = f.filter(m, event)
-            if m.nil?
-              break
-            end
-          end
-          m
-        }.compact
+    def filter(m, ev)
+      @filters.each do |f|
+        m = f.filter(m, ev)
+        if m.nil?
+          break
+        end
       end
-      each {|l| l.log(messages, event, level)}
+      m
     end
   end
 end